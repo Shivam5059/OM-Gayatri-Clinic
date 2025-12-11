70d16a792dc83e17da2c494b96b414c86c6a13ad
Priyal2104 <patelpriyal2104@gmail.com>
2025-12-11 16:11:35 +0530

    Delete Clinic directory

--- Clinic/Anand_Clinic/utils.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import redirect
-
-def group_required(group_name, login_url='login'):
-    def in_group(u):
-        return u.is_authenticated and u.groups.filter(name=group_name).exists()
-    return user_passes_test(in_group, login_url=login_url)
